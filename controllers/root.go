cfdd4e5a7e760b41c9d7e0b0c327fd8fbd10e4f8
ilyassatlassi <atlassiilyass@gmail.com>
2024-06-15 15:48:19 +0100

    chore: Refactor root handler to handle POST requests for /ascii-art endpoint

--- controllers/root.go
+++ controllers/root.go
@@ -25,11 +25,15 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 	var data Data
 	switch r.URL.Path {
 	case "/ascii-art":
-		postRequest(w, r, &data)
-		getPage(w, &data)
+		if r.Method == "POST" {
+			postRequest(w, r, &data)
+		} else {
+			error.HandleError(w, r, error.Error{Code: 405, Message: "Method not allowed!"})
+		}
+		// getPage(w, &data)
 	case "/":
 		if r.Method == "GET" {
-			getPage(w, &data)
+			getPage(w, r, &data)
 		} else {
 			error.HandleError(w, r, error.Error{Code: 405, Message: "Method not allowed!"})
 		}
@@ -39,35 +43,41 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func postRequest(w http.ResponseWriter, r *http.Request, data *Data) {
-	if r.Method == "POST" {
-		err_form := r.ParseForm()
-		if err_form != nil {
-			http.Error(w, err_form.Error(), http.StatusBadRequest)
-			return
-		}
-		data.Text = strings.ReplaceAll(r.FormValue("text"), "\r\n", "\\n")
-		data.Banner = r.FormValue("banner")
-		if !banners[data.Banner] {
-			data.Result = "Bad request banner not found! Error 400."
-			return
-		} else {
-			data.Result = fs.AsciiArtFs(data.Text, data.Banner)
-		}
+	tmpl, err := template.ParseFiles("views/base.html", "views/form.html")
+	if err != nil {
+		error.HandleError(w, r, error.Error{Code: 400, Message: "Bad request banner not found! "})
+
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	data.Text = strings.ReplaceAll(r.FormValue("text"), "\r\n", "\\n")
+	data.Banner = r.FormValue("banner")
+	if !banners[data.Banner] {
+		// data.Result = "Bad request banner not found! Error 400."
+		error.HandleError(w, r, error.Error{Code: 400, Message: "Bad request banner not found! "})
+		http.Error(w, "", http.StatusBadRequest)
+		return
 	} else {
-		error.HandleError(w, r, error.Error{Code: 405, Message: "Method not allowed!"})
+		data.Result = fs.AsciiArtFs(data.Text, data.Banner)
+	}
+	err = tmpl.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
 
-func getPage(w http.ResponseWriter, data *Data) {
+func getPage(w http.ResponseWriter, r *http.Request, data *Data) {
 	tmpl, err := template.ParseFiles("views/base.html", "views/form.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		error.HandleError(w, r, error.Error{Code: http.StatusInternalServerError, Message: err.Error()})
 		return
 	}
 	err = tmpl.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		error.HandleError(w, r, error.Error{Code: http.StatusInternalServerError, Message: err.Error()})
 		return
 	}
 }
--- pkg/errors/handle_error.go
+++ pkg/errors/handle_error.go
@@ -1,7 +1,6 @@
 package errors
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -15,13 +14,14 @@ func HandleError(w http.ResponseWriter, r *http.Request, error Error) {
 	tmpl, err := template.ParseFiles("views/errors/errors.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		fmt.Println(err.Error(), http.StatusInternalServerError)
+		// fmt.Println(err.Error(), http.StatusInternalServerError)
 		return
 	}
-	err = tmpl.Execute(w, error)
+	// err = tmpl.Execute(w, error)
+	err = tmpl.ExecuteTemplate(w, "error", error)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		fmt.Println(err.Error(), http.StatusInternalServerError)
+		// fmt.Println(err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
