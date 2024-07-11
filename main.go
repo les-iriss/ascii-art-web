d2452c57957b11991f00924c734b6a852850bacd
yassinerahhaui <yassinerahhaoui12@gmail.com>
2024-07-11 18:41:10 +0100

    edit main.go

--- main.go
+++ main.go
@@ -9,13 +9,15 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", controller.GetRequest)
-	http.HandleFunc("/ascii-art", controller.PostRequest)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", controller.GetRequest)
+	mux.HandleFunc("/ascii-art", controller.PostRequest)
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	PORT := ":8000"
 	fmt.Printf("Server running on http://localhost%s\n", PORT)
 
-	if err := http.ListenAndServe(PORT, nil); err != nil {
+	if err := http.ListenAndServe(PORT, mux); err != nil {
 		log.Fatalf("Failed to start server on %s: %v", PORT, err)
 	}
 }
