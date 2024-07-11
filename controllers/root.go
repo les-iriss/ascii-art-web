72860d451601a330e82ee38c66dff96974dc64f5
yassinerahhaui <yassinerahhaoui12@gmail.com>
2024-07-11 19:10:23 +0100

    handle error page not found

--- controllers/root.go
+++ controllers/root.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"strings"
 
-	error "ascii-art-web/pkg/errors"
+	errors "ascii-art-web/pkg/errors"
 	fs "ascii-art-web/pkg/fs"
 )
 
@@ -24,32 +24,36 @@ var banners = map[string]bool{
 func executeTemplate(w http.ResponseWriter, r *http.Request, data interface{}) {
 	templates, err := template.ParseFiles("views/base.html", "views/form.html")
 	if err != nil {
-		error.HandleError(w, r, error.Error{Code: http.StatusInternalServerError, Message: err.Error()})
+		errors.HandleError(w, r, errors.Error{Code: http.StatusInternalServerError, Message: err.Error()})
 	}
 	if err := templates.ExecuteTemplate(w, "base", data); err != nil {
-		error.HandleError(w, r, error.Error{Code: http.StatusInternalServerError, Message: err.Error()})
+		errors.HandleError(w, r, errors.Error{Code: http.StatusInternalServerError, Message: err.Error()})
 	}
 }
 
 func PostRequest(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		error.HandleError(w, r, error.Error{Code: http.StatusMethodNotAllowed, Message: "Method not allowed!"})
+		errors.HandleError(w, r, errors.Error{Code: http.StatusMethodNotAllowed, Message: "Method not allowed!"})
 		return
 	}
 	var data Data
 	data.Text = strings.ReplaceAll(r.FormValue("text"), "\r\n", "\\n")
 	data.Banner = r.FormValue("banner")
+	if !banners[data.Banner] {
+		errors.HandleError(w, r, errors.Error{Code: http.StatusBadRequest, Message: "Bad request invalid banner! "})
+		return
+	}
 	if data.Text == "" {
-		error.HandleError(w, r, error.Error{Code: http.StatusBadRequest, Message: "Bad request empty text! "})
+		errors.HandleError(w, r, errors.Error{Code: http.StatusBadRequest, Message: "Bad request empty text! "})
 		return
 	}
 	if len(data.Text) > 250 {
-		error.HandleError(w, r, error.Error{Code: http.StatusBadRequest, Message: "Bad request! You exceeded the length limit."})
+		errors.HandleError(w, r, errors.Error{Code: http.StatusBadRequest, Message: "Bad request! You exceeded the length limit."})
 		return
 
 	}
 	if !banners[data.Banner] {
-		error.HandleError(w, r, error.Error{Code: http.StatusBadRequest, Message: "Bad request banner not found! "})
+		errors.HandleError(w, r, errors.Error{Code: http.StatusBadRequest, Message: "Bad request banner not found! "})
 		return
 	}
 
@@ -58,8 +62,12 @@ func PostRequest(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetRequest(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		errors.HandleError(w, r, errors.Error{Code: http.StatusNotFound, Message: "Page not found!"})
+		return
+	}
 	if r.Method != http.MethodGet {
-		error.HandleError(w, r, error.Error{Code: http.StatusMethodNotAllowed, Message: "Method not allowed!"})
+		errors.HandleError(w, r, errors.Error{Code: http.StatusMethodNotAllowed, Message: "Method not allowed!"})
 		return
 	}
 	executeTemplate(w, r, nil)
